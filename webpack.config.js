a29bb64337410b9539885996fafc7ccbcd696abd
dileya <cujaia22@gmail.com>
2019-07-31 21:54:02 +0300

    hh

--- webpack.config.js
+++ webpack.config.js
@@ -40,14 +40,38 @@ module.exports = (env, argv) => {
 					]
 				},
 				{
-					test: /\.(png|jpg)$/,
+					test: /\.(gif|png|jpe?g|svg)$/i,
 					use: [{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'images'
-						}
-					}]					
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]',
+							}
+						},
+						{
+							loader: 'image-webpack-loader',
+							options: {
+								mozjpeg: {
+									progressive: true,
+									quality: 65
+								},
+								// optipng.enabled: false will disable optipng
+								optipng: {
+									enabled: false,
+								},
+								pngquant: {
+									quality: '65-90',
+									speed: 4
+								},
+								gifsicle: {
+									interlaced: false,
+								},
+								// the webp option will enable WEBP
+								webp: {
+									quality: 75
+								}
+							}
+						},
+					],
 				}
 			]
 		},
